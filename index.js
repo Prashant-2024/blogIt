e7cd6e25b3c997e9eb199c50891991448858f3d4
Prashant Solanki <prashantsolanki2025@gmail.com>
2024-06-11 18:43:05 +0530

    Routes Configure on Submit, Post Creation Function

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
+import bodyParser from "body-parser";
 import express from "express";
 
 const app = express();
 const port = 3000;
-
+var newBlog;
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
-})
 
+function addBlog(req, res, next) {
+  newBlog = req.body["blog"];
+  next();
+}
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(addBlog);
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+    console.log(newBlog);
+  res.render("index.ejs", { blogs: newBlog });
+});
 
 app.listen(port, (error) => {
   if (error) throw error;
